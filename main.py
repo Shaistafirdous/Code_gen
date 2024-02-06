9d3bcfa0c754a5e185e5289404501855570a8082
Shaistafirdous <138618600+Shaistafirdous@users.noreply.github.com>
2024-02-06 11:06:07 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import openai
+from openai import OpenAI
+
+# Replace with your actual OpenAI API key
+client = OpenAI(api_key=("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"))
+
+question = st.text_input("Type your question here...")
+with st.sidebar:
+    st. title("Get your text converted to Code")
+if question:
+    response = client.chat.completions.create(
+        model="ft:gpt-3.5-turbo-0613:personal::8p1RYc64",
+        messages=[{"role": "user", "content": question}],
+    )
+    generated_code = response.choices[0].message.content
+
+    # Generate algorithm
+    prompt = f"""Your AI assistant's task is to produce a step-by-step algorithm for the code delimited by ```\
+    and also convert the code generated to an understandable manner according to the question delimited by ``\
+    ``` code:{generated_code} ```
+    `` question:{question} ``
+    """
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": prompt}],
+    )
+
+    algorithm = completion.choices[0].message.content
+
+    # Create two columns
+    col1, col2 = st.columns(2)
+
+    # Display algorithm in the first column
+    col1.header("Algorithm")
+    col1.write(algorithm)
+
+    # Display generated code in the second column
+    col2.header("Generated Code")
+    col2.code(generated_code, language="python")
