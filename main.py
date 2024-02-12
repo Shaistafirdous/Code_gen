a8011e032d3af60adbc8545367aa876ad6dd75bc
Shaistafirdous <138618600+Shaistafirdous@users.noreply.github.com>
2024-02-13 01:32:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,23 @@ import streamlit as st
 import openai
 from openai import OpenAI
 import black
+import transformers
+from transformers import AutoTokenizer
+from transformers import TFAutoModelForSeq2SeqLM, DataCollatorForSeq2Seq
+from transformers import AdamWeightDecay
+from transformers import AutoTokenizer, TFAutoModelForSeq2SeqLM
 
+model_checkpoint = "Helsinki-NLP/opus-mt-en-hi"
+tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
+
+from transformers import TFAutoModelForSeq2SeqLM
+
+model_path = "C:/Users/Vishwas/Desktop/personal/major project/firdous/tf_model/"
+model = TFAutoModelForSeq2SeqLM.from_pretrained(model_path)
+
+OPENAI_API_KEY='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'
 # Replace with your actual OpenAI API key
-client = OpenAI(api_key=st.secrets.get("OPENAI_API_KEY"))
+client = OpenAI(api_key=OPENAI_API_KEY)
 
 question = st.text_input("Type your question here...")
 with st.sidebar:
@@ -16,18 +30,28 @@ if question:
     )
     generated_code = response.choices[0].message.content
 
-    formatted_code = black.format_str(generated_code, mode=black.FileMode())
+    #formatted_code = black.format_str(generated_code, mode=black.FileMode())
 
     # Generate algorithm
-    prompt = f""" Your AI assistant's task is to produce a step-by-step algorithm for the code delimited by ```\
+    prompt = f""" Your AI assistant's task is to produce a explaination for the code delimited by ``` which can further be used for language translation also\
     ``` code:{generated_code} ``` """
     
     completion = client.chat.completions.create(
-        model="gpt-3.5-turbo",
+        model="gpt-3.5-turbo-0125",
         messages=[{"role": "user", "content": prompt}],
     )
 
     algorithm = completion.choices[0].message.content
+
+    if st.button("Translate"):
+            tokenized = tokenizer([algorithm], return_tensors='np')
+            out = model.generate(**tokenized, max_length=218)
+            with tokenizer.as_target_tokenizer():
+                translated_algorithm = tokenizer.decode(out[0], skip_special_tokens=True)
+
+            # Display translated algorithm in the same column
+            st.header("Translated Algorithm")
+            st.write(translated_algorithm)
 
     # Create two columns
     col1, col2 = st.columns(2)
@@ -38,4 +62,4 @@ if question:
 
     # Display generated code in the second column
     col2.header("Generated Code")
-    col2.code(formatted_code, language="python")
+    col2.code(generated_code, language="python")
